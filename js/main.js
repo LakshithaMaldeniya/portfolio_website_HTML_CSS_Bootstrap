09fe4f11bf1d297d7fe2f8b147005e90ab368296
LakshithaMaldeniya <rslakshithamaldeniya@gmail.com>
2023-06-18 00:43:37 +0530

    contact page developed

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function reverl(){
     for (let i=0; i<reverl.length;i++){
         var windowheight = window.innerHeight;
         var reverltop = reverl[i].getBoundingClientRect().top;
-        var reverlpoint =100;
+        var reverlpoint =1;
 
         if (reverltop <windowheight-reverlpoint){
             reverl[i].classList.add('active');
@@ -26,7 +26,7 @@ function reverl2(){
     for (let i=0; i<reverl2.length;i++){
         var windowheight2 = window.innerHeight;
         var reverltop2 = reverl2[i].getBoundingClientRect().top;
-        var reverlpoint2 =80;
+        var reverlpoint2 =1;
 
         if (reverltop2 <windowheight2-reverlpoint2){
             reverl2[i].classList.add('active2');
